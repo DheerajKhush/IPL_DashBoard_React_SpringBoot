a3e483a6e872a7cb05961a6fe1391b57775e0ff0
DheerajKhush <Dheerajkhushalani2000@gmail.com>
2021-09-04 10:23:14 +0530

    completed  ui component for Team Page

--- src/frontend/my-app/src/components/MatchSmallCard.js
+++ src/frontend/my-app/src/components/MatchSmallCard.js
@@ -1,10 +1,14 @@
 import{React} from 'react';
-
-export const MatchSmallCard =({match}) => {
+import {Link} from 'react-router-dom'
+export const MatchSmallCard =({teamName, match}) => {
   if(!match) return null;
+  const otherTeam=match.team1===teamName ? match.team2:match.team1;
+  const otherTeamRoute=`/teams/${otherTeam}`;
   return (
     <div className="MatchSmallCard">
-      <p>{match.team1} vs {match.team2}</p>
+      <h3>vs 
+        <Link to={otherTeamRoute}>{otherTeam}</Link></h3>
+      <p>{match.matchWinner} won by {match.result_margin} {match.result}</p>
     </div>
   );
 }
---
+++ src/frontend/my-app/src/pages/MatchPage.js
@@ -0,0 +1,12 @@
+import { React, useEffect, useState } from 'react';
+
+
+export const MatchPage = () => {
+
+  
+  return (
+    <div className="TeamPage">
+      <h1>Match Page</h1>
+    </div>
+  );
+}
